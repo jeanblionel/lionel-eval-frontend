754c01e7b56d3c95d4ed482fe4c3934ceb9e79b5
jeanblionel <jeanblionel@gmail.com>
2023-11-24 15:37:53 +0100

    Add menu + footer, form finished, start main

--- assets/script.js
+++ assets/script.js
@@ -34,4 +34,58 @@ const updateSlide = ()=>{
 const interval = 5000
 const autoSlider = setInterval(()=>{
    moveSlide (1);
-}, interval)
+}, interval)
+
+//////////////////
+
+const carsLocation = [
+    {
+        "id": 10,
+        "image": "assets/images/vehicule1.png",
+        "model":"peugeot",
+        "description": "bla bla",
+        "price": 900,
+        "agence": "Paris",
+        "thumbnails": [
+            "vignette1.jpg",
+            "vignette2.jpg"
+        ]
+    },
+    {
+        "id": 11,
+        "image": "vehicule2.png",
+        "model":"ford",
+        "description": "bla bla",
+        "price": 990,
+        "agence": "Paris",
+        "thumbnails": [
+            "vignette1.jpg",
+            "vignette2.jpg"
+        ]
+    },
+    {
+        "id": 13,
+        "image": "vehicule3.jpg",
+        "model":"audi",
+        "description": "bla bla",
+        "price": 600,
+        "agence": "Paris",
+        "thumbnails": [
+            "vignette1.jpg",
+            "vignette2.jpg"
+        ]
+    },
+    {
+        "id": 14,
+        "image": "vehicule4.jpg",
+        "model":"opel",
+        "description": "bla bla",
+        "price": 700,
+        "agence": "Paris",
+        "thumbnails": [
+            "vignette1.jpg",
+            "vignette2.jpg"
+        ]
+    }
+
+]
